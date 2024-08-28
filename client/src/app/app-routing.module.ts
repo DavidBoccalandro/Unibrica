da1cc12cff589910a9ccddb09add23f43ce58eb8
DavidBoccalandro <davidboccalandro@hotmail.com>
2024-08-28 11:20:07 -0300

    add redirects to login when is not logged and dashboard when is logged + update login-decor component

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { LoginComponent } from './login/login.component';
+import { AuthGuard } from './core/guards/auth.guard';
 import { canActivateLogin } from './core/guards/login.guard';
 import { ErrorPageComponent } from './error-page/error-page.component';
 
@@ -8,19 +8,20 @@ const routes: Routes = [
   {
     path: 'login',
     loadChildren: () =>
-    import('./login/login.module').then(
-      m => m.LoginModule
-    ),
+      import('./login/login.module').then(
+        m => m.LoginModule
+      ),
     canActivate: [canActivateLogin]
   },
   {
     path: '',
-    loadChildren: () => 
-    import('./layout/layout.module').then(
-      m => m.LayoutModule
-    )
+    loadChildren: () =>
+      import('./layout/layout.module').then(
+        m => m.LayoutModule
+      ),
+    canActivate: [AuthGuard]
   },
-  { path: '**', component: ErrorPageComponent}
+  { path: '**', component: ErrorPageComponent }
 ];
 
 @NgModule({
--- client/src/app/core/authentication/auth-store/auth.reducers.ts
+++ client/src/app/core/authentication/auth-store/auth.reducers.ts
@@ -1,7 +1,7 @@
 import { createReducer, on } from '@ngrx/store';
 
-import { login, loginSuccess, loginFailure, clearAuth } from './auth.actions';
 import { AuthState } from '../auth.interfaces';
+import { clearAuth, login, loginFailure, loginSuccess } from './auth.actions';
 
 
 export const initialAuthState: AuthState = {
@@ -11,8 +11,8 @@ export const initialAuthState: AuthState = {
 };
 
 export const AuthReducer = createReducer(initialAuthState,
-  on(login, state => ({ ...state, isLoading: true })), 
-  on(loginSuccess, (state, { user }) => ({ ...state, error:null, user, isLoading: false })),
-  on(loginFailure, (state, { error }) => ({ error, token:null, isLoading: false, user:null })),
+  on(login, state => ({ ...state, isLoading: true })),
+  on(loginSuccess, (state, { user }) => ({ ...state, error: null, user, isLoading: false })),
+  on(loginFailure, (_state, { error }) => ({ error, token: null, isLoading: false, user: null })),
   on(clearAuth, () => (initialAuthState))
 );
