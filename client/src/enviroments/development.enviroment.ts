d139efb29bcdc025c0253176ba5dee3fb9d2b2d3
Martin <aerocmc@gmail.comn>
2024-08-30 09:07:18 -0300

    Se configuró el env production en client (ng serve => dev; ng build => prod)

--- client/src/enviroments/development.enviroment.ts
+++ client/src/enviroments/development.enviroment.ts
@@ -2,6 +2,6 @@ export const environment = {
     production: false,
     baseUrl: 'http://localhost:4200',
     envVar: {
-        API_URL:'https://unibrica-9dv1.onrender.com/api'
+        API_URL:'http://localhost:8000/api'
     }
   };
