fce13347bdcb0821016f8cbab44fbeb823fd751e
Martin <aerocmc@gmail.comn>
2024-09-16 19:49:54 -0300

    Se añadió ruta para obtener todos los RepeatedDebtor

--- server/src/repeated-debtor/controller/repeated-debtor.controller.ts
+++ server/src/repeated-debtor/controller/repeated-debtor.controller.ts
@@ -1,4 +1,29 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Query } from '@nestjs/common';
+import { RepeatedDebtorService } from '../services/repeated-debtor.service';
 
-@Controller('repeated-debtor')
-export class RepeatedDebtorController {}
+@Controller('repeated-debtors')
+export class RepeatedDebtorController {
+  constructor(private repeatedDebtorRepository: RepeatedDebtorService) {}
+
+  @Get('all')
+  public async getAllRepeatedDebtors(
+    @Query('limit') limit: number,
+    @Query('offset') offset: number,
+    @Query('stringFilters') stringFilters: string,
+    @Query('numericFilters') numericFilters: string,
+    @Query('dateFilters') dateFilters: string
+  ) {
+    let parsedStringFilters, parsedNumericFilters, parsedDateFilters;
+    if (stringFilters && stringFilters !== 'undefined') {
+      parsedStringFilters = JSON.parse(stringFilters);
+    }
+    if (numericFilters && numericFilters !== 'undefined') {
+      parsedNumericFilters = JSON.parse(numericFilters);
+    }
+    if (dateFilters && dateFilters !== 'undefined') {
+      parsedDateFilters = JSON.parse(dateFilters);
+    }
+
+    return this.repeatedDebtorRepository.getAllRepeatedDebtors();
+  }
+}
--- server/src/repeated-debtor/services/repeated-debtor.service.ts
+++ server/src/repeated-debtor/services/repeated-debtor.service.ts
@@ -1,4 +1,16 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { RepeatedDebtorEntity } from '../entities/repeated-debtor.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
-export class RepeatedDebtorService {}
+export class RepeatedDebtorService {
+  constructor(
+    @InjectRepository(RepeatedDebtorEntity)
+    private repeatedDebtorRepository: Repository<RepeatedDebtorEntity>
+  ) {}
+
+  async getAllRepeatedDebtors() {
+    return await this.repeatedDebtorRepository.find({ relations: { debtor: true } });
+  }
+}
